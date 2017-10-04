8f1975a27673cc50402cee02edee1022045c782e
muiezhimura <muiezhimura@gmail.com>
2017-10-05 05:54:28 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/uny/unyapps/IndexBerita.java
@@ -0,0 +1,13 @@
+package id.ac.uny.unyapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndexBerita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_berita);
+    }
+}
